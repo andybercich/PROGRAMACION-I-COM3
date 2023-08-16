302fe50ae9ecd5d6078323981a9ec2251bdd592a
andybercich <andybercich6@gmail.com>
2023-08-16 10:14:59 -0300

    PRIMER ENTREGA

---
+++ ejercicios_complementario.py
@@ -0,0 +1,54 @@
+# CREANDO VARIABLE LLAMADA numero1
+
+numero1= 24
+# CREANDO VARIABLE LLAMADA numero2
+numero2 = 12.4
+# CREANDO VARIABLE SUMA
+suma = numero1+numero2
+# CREANDO VARIABLES RESTA,MULTIPLICACIÓN Y DIVISIÓN, LUEGO LAS MUESTRO EN CONSOLA
+multiplicacion = numero2 * numero1
+division = numero1 / numero2
+resta = numero1 - numero2
+
+print( F"RESTA: {resta} MULTIPLICACIÓN: {multiplicacion} DIVISIÓN: {division}")
+
+# CREANDO VARIABLE NOMBRE
+
+nombre = "ANDRÉS"
+
+#CREANDO VARIABLE PRECIO
+
+precio = 10.50
+
+# VARIABLE DESCUENTO
+
+descuento = 0.50
+
+# VARIABLE precio_final
+
+precio_final = precio - (precio*descuento)
+
+# VARIABLE CADENA
+
+cadena = "TRES TRISTES TIGRES TRAGABAN TRIGO EN UN TRIGAL"
+
+# CONTANDO LOS CARÁCTERES DE LA CADENA
+
+longitud = print(len(cadena))
+
+# CREANDO OTRA VEZ LA VARIABLE PRECIO
+
+precio = int(50.25)
+
+# CONCATENANDO LAS VARIABLES NOMBRE Y APELLIDO
+
+apellido ="BERCICH"
+
+nombre_completo = apellido + " "+ nombre
+print(nombre_completo)
+
+# INCREMENTANDO MI EDAD EN 5 Y LUEGO DISMINUYÉNDOLA EN 10
+
+edad = 19
+edad += 5
+edad -=10
